75780a517586a5ca28abee744473ca0ad5fd755e
--replace-all <surya.indian321@gmail.com>
2024-10-07 21:49:51 +0530

    Model Added

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,12 +1,12 @@
 const asyncHandler = (requestHandler) => {
   (req, res, next) => {
-    Promise.resolve(requestHandler(req, res, next)).catch((error) => next(err));
+    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+      next(error)
+    );
   };
 };
 
 export { asyncHandler };
-
-
 
 // const asyncHandler = () => {};
 // const asyncHandler = (fn) => () => {};
